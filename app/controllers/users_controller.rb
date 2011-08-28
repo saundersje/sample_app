7a3ddf63c40d529e8ce769f5b2ff6017a8686cab
John Saunders <saundersje@gmail.com>
2011-08-28 17:08:04 -0500

    8.6 Exercises

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,8 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       @title = "Sign Up"
+      @user.password = nil
+      @user.password_confirmation = nil
       render 'new'
     end
   end
